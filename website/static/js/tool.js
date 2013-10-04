e92c875e5311cb2a8143687f391dd783fa9dfa27
jywsn <jennifer.y.wilson@northwestern.edu>
2013-10-04 13:48:01 -0500

    sortable slides

--- website/static/js/tool.js
+++ website/static/js/tool.js
@@ -1,4 +1,8 @@
 
+Array.prototype.move = function(from, to) {
+    this.splice(to, 0, this.splice(from, 1)[0]);
+};
+
 function parseQuerystring() {
     var nvpair = {};
     var qs = window.location.search.replace('?', '');
