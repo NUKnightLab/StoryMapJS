b1828f0a334bb601f9c49271d4069db1f792199a
Zach Wise <zach@digitalartwork.net>
2013-12-17 11:10:58 -0600

    Update language handling

---
+++ compiled/js/locale/es.js
@@ -0,0 +1,1 @@
+VCO.Language={name:"Spanish",lang:"es",messages:{loading:"cargando",wikipedia:"From Wikipedia, the free encyclopedia"},buttons:{map_overview:"vista general del mapa",backtostart:"volver al comienzo",collapse_toggle:"ocultar mapa",uncollapse_toggle:"mostrar mapa"}};
