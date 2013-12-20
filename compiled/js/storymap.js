c916a10db37e3c4b4f6698b23ebad6eb5f8b64da
Zach Wise <zach@digitalartwork.net>
2013-12-20 15:22:10 -0600

    Update twitter styling

--- compiled/js/storymap.js
+++ compiled/js/storymap.js
@@ -5219,6 +5219,7 @@ VCO.Media.Twitter = VCO.Media.extend({
 		
 		// API URL
 		api_url = "http://api.twitter.com/1/statuses/oembed.json?id=" + this.media_id + "&omit_script=true&include_entities=true&callback=?";
+		//api_url = "http://api.twitter.com/1/statuses/oembed.json?id=" + this.media_id + "&include_entities=true&callback=?";
 		
 		// API Call
 		VCO.getJSON(api_url, function(d) {
@@ -5228,21 +5229,40 @@ VCO.Media.Twitter = VCO.Media.extend({
 	},
 	
 	createMedia: function(d) {		
-		var tweet		= "",
-			tweetuser	= "";
+		var tweet				= "",
+			tweet_text			= "",
+			tweetuser			= "",
+			tweet_status_temp 	= "",
+			tweet_status_url 	= "",
+			tweet_status_date 	= "";
 			
 		//	TWEET CONTENT
-		tweet += d.html.split("<\/p>\&mdash;")[0] + "</p></blockquote>";
-		tweetuser = d.author_url.split("twitter.com\/")[1];
+		tweet_text 			= d.html.split("<\/p>\&mdash;")[0] + "</p></blockquote>";
+		tweetuser			= d.author_url.split("twitter.com\/")[1];
+		tweet_status_temp 	= d.html.split("<\/p>\&mdash;")[1].split("<a href=\"")[1];
+		tweet_status_url 	= tweet_status_temp.split("\"\>")[0];
+		tweet_status_date 	= tweet_status_temp.split("\"\>")[1].split("<\/a>")[0];
+		
+		trace("tweet_status_url " + tweet_status_url);
+		trace("tweet_status_date " + tweet_status_date);
+		
+		// 	TWEET CONTENT
+		tweet += tweet_text;
+		
+		//tweet += "</blockquote>";
 		
 		//	TWEET AUTHOR
-		tweet += "<div class='vcard author'>";
+		tweet += "<div class='vcard'>";
+		tweet += "<a href='" + tweet_status_url + "' class='twitter-date' target='_blank'>" + tweet_status_date + "</a>";
+		tweet += "<div class='author'>";
 		tweet += "<a class='screen-name url' href='" + d.author_url + "' target='_blank'>";
 		tweet += "<span class='avatar'></span>";
-		tweet += "<span class='fn'>" + d.author_name + "</span>";
+		tweet += "<span class='fn'>" + d.author_name + " <span class='vco-icon-twitter'></span></span>";
 		tweet += "<span class='nickname'>@" + tweetuser + "<span class='thumbnail-inline'></span></span>";
 		tweet += "</a>";
 		tweet += "</div>";
+		tweet += "</div>";
+		
 		
 		// Add to DOM
 		this._el.content_item.innerHTML	= tweet;
