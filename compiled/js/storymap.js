8cc15dea619633d2a8afb022de9c149e920ba22d
Zach Wise <zach@digitalartwork.net>
2013-12-20 15:22:50 -0600

    Merged

--- compiled/js/storymap.js
+++ compiled/js/storymap.js
@@ -9792,6 +9792,9 @@ L.TileLayer = L.Class.extend({
 	},
 
 	_getSubdomain: function (tilePoint) {
+		if (!this.options.subdomains || this.options.subdomains.length == 0) {
+			return '';
+		}
 		var index = Math.abs(tilePoint.x + tilePoint.y) % this.options.subdomains.length;
 		return this.options.subdomains[index];
 	},
