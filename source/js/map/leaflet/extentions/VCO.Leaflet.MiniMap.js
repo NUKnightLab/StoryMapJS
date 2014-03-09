d649384edde5c298b4dc7405561233a0733b18d4
Zach Wise <zach@digitalartwork.net>
2014-03-08 22:37:49 -0600

    Cleanup console log

--- source/js/map/leaflet/extentions/VCO.Leaflet.MiniMap.js
+++ source/js/map/leaflet/extentions/VCO.Leaflet.MiniMap.js
@@ -231,7 +231,6 @@ L.Control.MiniMap = L.Control.extend({
     },
 
     _decideZoom: function(fromMaintoMini) {
-		trace("decideZOOM")
         if (!this.options.zoomLevelFixed && this.options.zoomLevelFixed != 0) {
             if (fromMaintoMini) {
 				return this._mainMap.getZoom() + this.options.zoomLevelOffset;
