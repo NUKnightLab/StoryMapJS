b077a5dba84cbc7b3fd37e276be1635301c79d99
jywsn <jennifer.y.wilson@northwestern.edu>
2014-03-19 11:25:55 -0500

    Comment out project-specific secrets import warning (not needed)

--- website/core/settings/loc.py
+++ website/core/settings/loc.py
@@ -3,15 +3,14 @@ import sys
 from os.path import normpath, join
 from .base import *
 
-# Import secrets
-sys.path.append(
-    abspath(join(PROJECT_ROOT, '../secrets/storymapjs/stg'))
-)
-
-try:
-    from secrets import *
-except ImportError, e:
-    print 'WARNING: Could not import project secrets (%s).  You will not be able to deploy.' % (e)
+# Import secrets (no project-specific secrets)
+#sys.path.append(
+#    abspath(join(PROJECT_ROOT, '../secrets/storymapjs/stg'))
+#)
+#try:
+#    from secrets import *
+#except ImportError, e:
+#    print 'WARNING: Could not import project secrets (%s).  You will not be able to deploy.' % (e)
 
 # Set static URL
 STATIC_URL = '/static'
