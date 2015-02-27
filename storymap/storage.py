bebc128d8756598afc9e682ce295656a15d2a535
jywsn <jennifer.y.wilson@northwestern.edu>
2015-02-27 16:36:06 -0600

    Trap and reraise S3 errors

--- storymap/storage.py
+++ storymap/storage.py
@@ -8,6 +8,7 @@ import os
 import sys
 import time
 import json
+from functools import wraps
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from boto.exception import S3ResponseError
@@ -20,6 +21,17 @@ _conn = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
 _bucket = _conn.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
 
 
+def _reraise_s3response(f):
+    """Decorator trap and re-raise S3ResponseError's"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except S3ResponseError, e:
+            raise Exception(e.message)
+    return decorated_function
+
+    
 def key_id():
     """
     Get id for key
@@ -32,6 +44,7 @@ def key_prefix(*args):
 def key_name(*args):
     return '%s/%s' % (settings.AWS_STORAGE_BUCKET_KEY, '/'.join(args))
                  
+@_reraise_s3response 
 def list_keys(key_prefix, n, marker=''):
     """
     List keys that start with key_prefix (<> key_prefix itself)
@@ -49,6 +62,7 @@ def list_keys(key_prefix, n, marker=''):
         key_list.append(item)
     return key_list, (i == n)
 
+@_reraise_s3response 
 def list_key_names(key_prefix, n, marker=''):
     """
     List key names that start with key_prefix (<> key_prefix itself)
@@ -66,6 +80,7 @@ def list_key_names(key_prefix, n, marker=''):
         name_list.append(item.name)
     return name_list, (i == n)
 
+@_reraise_s3response 
 def copy_key(src_key_name, dst_key_name):
     """
     Copy from src_key_name to dst_key_name
@@ -73,6 +88,7 @@ def copy_key(src_key_name, dst_key_name):
     dst_key = _bucket.copy_key(dst_key_name, _bucket.name, src_key_name)
     dst_key.set_acl('public-read')
 
+@_reraise_s3response 
 def save_from_data(key_name, content_type, content):
     """
     Save content with content-type to key_name
@@ -83,13 +99,15 @@ def save_from_data(key_name, content_type, content):
         key.content_type = content_type
     key.set_contents_from_string(content, policy='public-read')
         
+@_reraise_s3response 
 def save_from_url(key_name, url):
     """
     Save file at url to key_name
     """
     r = requests.get(url)
     save_from_data(key_name, r.headers['content-type'], r.content) 
-    
+   
+@_reraise_s3response 
 def load_json(key_name):
     """
     Get contents of key as json
@@ -97,7 +115,8 @@ def load_json(key_name):
     key = Key(_bucket, key_name)
     contents = key.get_contents_as_string()
     return json.loads(contents)
-
+ 
+@_reraise_s3response 
 def save_json(key_name, data):
     """
     Save data to key_name as json
@@ -108,6 +127,7 @@ def save_json(key_name, data):
         content = json.dumps(data)
     save_from_data(key_name, 'application/json', content)
         
+@_reraise_s3response 
 def delete(key_name):
     """
     Delete key
