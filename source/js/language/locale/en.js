3da4547c282a33cdb76ebdd2e3daf361a1024bce
Luke Rague <luke.rague@gmail.com>
2015-03-26 21:09:43 +0000

    removed necessity for en.js

--- source/js/language/locale/en.js
+++
@@ -1,1 +0,0 @@
-VCO.Language={name:"English",lang:"en",messages:{loading:"Loading",wikipedia:"From Wikipedia, the free encyclopedia",start:"Start Exploring"},buttons:{map_overview:"Map Overview",overview:"Overview",backtostart:"Back To Beginning",collapse_toggle:"Hide Map",uncollapse_toggle:"Show Map"}}
