ef8a3f5ba42030c4b7a4a875c48f1589cae5e359
Scott Bradley <scott.b.bradley@gmail.com>
2015-06-05 10:36:15 -0500

    removes build directory

--- build/js/locale/he.js
+++
@@ -1,1 +0,0 @@
-VCO.Language={name:"עברית",lang:"he",messages:{loading:"טוען",wikipedia:"מתוך ויקיפדיה, האנציקלופדיה החופשית",start:"התחל במסע"},buttons:{map_overview:"המפה במלואה",overview:"תצוגה מקדימה",backtostart:"חזור להתחלה",collapse_toggle:"הסתר מפה",uncollapse_toggle:"הצג מפה"}};
