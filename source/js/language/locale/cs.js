b03b1fa3772f8914a83b98a6b5553709563faebc
Miloš Kroulík <milos.kroulik@gmail.com>
2015-06-05 21:23:23 +0200

    Create cs.js
    Adds Czech language

---
+++ source/js/language/locale/cs.js
@@ -0,0 +1,16 @@
+VCO.Language = {
+    name:                   "Čeština",
+    lang:                   "cs",
+    messages: {
+        loading:            "Nahrávání",
+        wikipedia:          "Z Wikipedie, svobodné encyklopedie",
+        start:              "Prozkoumat"
+    },
+    buttons: {
+        map_overview:       "Přehledová mapa",
+        overview:           "Přehled",
+        backtostart:        "Zpět na začátek",
+        collapse_toggle:    "Skrýt mapu",
+        uncollapse_toggle:  "Zobrazit mapu"
+    }
+}
