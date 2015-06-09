355573732b6032989b61ae63b7c31bd3cea80930
Gaute Hvoslef Kvalnes <gaute@verdsveven.com>
2015-06-09 15:24:16 +0200

    Added Norwegian Nynorsk translation.

---
+++ source/js/language/locale/nn.js
@@ -0,0 +1,16 @@
+VCO.Language = {
+    name: "Norsk nynorsk",
+    lang: "nn",
+    messages: {
+        loading: "Lastar inn",
+        wikipedia: "frå Wikipedia, det frie oppslagsverket",
+	start: "Utforsk"
+    },
+    buttons: {
+        map_overview:      "Sjå oversiktskart",
+	overview:          "Sjå oversiktskart",
+        backtostart:       "Til starten",
+        collapse_toggle:   "Gøym kartet",
+        uncollapse_toggle: "Vis kartet"
+    }
+};
