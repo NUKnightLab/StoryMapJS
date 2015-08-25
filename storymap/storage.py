908e2bd0f718cd3714fdcc197c8cb8abd267541f
Joe Germuska <joe@germuska.com>
2015-08-25 09:30:09 -0500

    update storage.py to fix SSL error (https://github.com/boto/boto/issues/2836)

--- storymap/storage.py
+++ storymap/storage.py
@@ -10,7 +10,8 @@ import time
 import traceback
 import json
 from functools import wraps
-from boto.s3.connection import S3Connection
+from boto.s3.connection import S3Connection, OrdinaryCallingFormat
+
 from boto.s3.key import Key
 from boto.exception import S3ResponseError
 import requests
@@ -18,7 +19,7 @@ import requests
 # Get settings module
 settings = sys.modules[os.environ['FLASK_SETTINGS_MODULE']]
 
-_conn = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)    
+_conn = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY,calling_format=OrdinaryCallingFormat())    
 _bucket = _conn.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
 
 class StorageException(Exception):
@@ -28,8 +29,8 @@ class StorageException(Exception):
     def __init__(self, message, detail):
         super(Exception, self).__init__(message)
         self.detail = detail
-    
-    
+
+
 def _reraise_s3response(f):
     """Decorator trap and re-raise S3ResponseError as StorageException"""
     @wraps(f)
@@ -41,34 +42,34 @@ def _reraise_s3response(f):
             raise StorageException(e.message, e.body)
     return decorated_function
 
-    
+
 def key_id():
     """
     Get id for key
     """
     return repr(time.time())
-    
+
 def key_prefix(*args):
     return '%s/%s/' % (settings.AWS_STORAGE_BUCKET_KEY, '/'.join(args))
 
 def key_name(*args):
     return '%s/%s' % (settings.AWS_STORAGE_BUCKET_KEY, '/'.join(args))
 
-        
-@_reraise_s3response 
+
+@_reraise_s3response
 def list_keys(key_prefix, n, marker=''):
     """
     List keys that start with key_prefix (<> key_prefix itself)
     @n = number of items to return
-    @marker = name of last item 
+    @marker = name of last item
     """
     key_list = []
     i = 0
-    
+
     for i, item in enumerate(_bucket.list(prefix=key_prefix, marker=marker)):
         if i == n:
             break
-        if item.name == key_prefix:           
+        if item.name == key_prefix:
             continue
         key_list.append(item)
     return key_list, (i == n)
@@ -76,30 +77,30 @@ def list_keys(key_prefix, n, marker=''):
 
 def all_keys():
     for item in _bucket.list(prefix=settings.AWS_STORAGE_BUCKET_KEY):
-        if item.name == key_prefix:           
+        if item.name == key_prefix:
             continue
         yield item.key
 
 
-@_reraise_s3response 
+@_reraise_s3response
 def list_key_names(key_prefix, n, marker=''):
     """
     List key names that start with key_prefix (<> key_prefix itself)
     @n = number of items to return
-    @marker = name of last item 
+    @marker = name of last item
     """
     name_list = []
     i = 0
-    
+
     for i, item in enumerate(_bucket.list(prefix=key_prefix, marker=marker)):
         if i == n:
             break
-        if item.name == key_prefix:           
+        if item.name == key_prefix:
             continue
         name_list.append(item.name)
     return name_list, (i == n)
 
-@_reraise_s3response 
+@_reraise_s3response
 def copy_key(src_key_name, dst_key_name):
     """
     Copy from src_key_name to dst_key_name
@@ -107,7 +108,7 @@ def copy_key(src_key_name, dst_key_name):
     dst_key = _bucket.copy_key(dst_key_name, _bucket.name, src_key_name)
     dst_key.set_acl('public-read')
 
-@_reraise_s3response 
+@_reraise_s3response
 def save_from_data(key_name, content_type, content):
     """
     Save content with content-type to key_name
@@ -117,16 +118,16 @@ def save_from_data(key_name, content_type, content):
         key = _bucket.new_key(key_name)
         key.content_type = content_type
     key.set_contents_from_string(content, policy='public-read')
-        
-@_reraise_s3response 
+
+@_reraise_s3response
 def save_from_url(key_name, url):
     """
     Save file at url to key_name
     """
     r = requests.get(url)
-    save_from_data(key_name, r.headers['content-type'], r.content) 
-   
-@_reraise_s3response 
+    save_from_data(key_name, r.headers['content-type'], r.content)
+
+@_reraise_s3response
 def load_json(key_name):
     """
     Get contents of key as json
@@ -134,8 +135,8 @@ def load_json(key_name):
     key = Key(_bucket, key_name)
     contents = key.get_contents_as_string()
     return json.loads(contents)
- 
-@_reraise_s3response 
+
+@_reraise_s3response
 def save_json(key_name, data):
     """
     Save data to key_name as json
@@ -145,13 +146,10 @@ def save_json(key_name, data):
     else:
         content = json.dumps(data)
     save_from_data(key_name, 'application/json', content)
-        
-@_reraise_s3response 
+
+@_reraise_s3response
 def delete(key_name):
     """
     Delete key
     """
     _bucket.delete_key(key_name)
-     
-
-    
