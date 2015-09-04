2dac5976b3be79ffc5d1c3aa63ee72958febeb77
Joe Germuska <joe@germuska.com>
2015-09-04 09:57:08 -0500

    add eesti (Estonian) l10n

---
+++ source/js/language/locale/et.js
@@ -0,0 +1,16 @@
+VCO.Language = {
+    name: "eesti",
+    lang: "et",
+     messages: {
+        loading: "Laadib",
+        wikipedia: "Vikipeedia, vaba entsüklopeedia",
+        start: "Uuri"
+    },
+    buttons: {
+        map_overview:      "Kaardi ülevaade",
+        overview:          "Ülevaade",
+        backtostart:       "Tagasi algusesse",
+        collapse_toggle:   "Peida kaart",
+        uncollapse_toggle: "Näita kaarti"
+    }
+};
