bbdc2af0c7dc58615f0e197309b6b213a61bcaec
Joe Germuska <joe@germuska.com>
2015-11-03 17:05:03 -0600

    add Portuguese

---
+++ source/js/language/locale/pt.js
@@ -0,0 +1,17 @@
+VCO.Language = {
+    name: "Português",
+    lang: "pt",
+    messages: {
+        loading: "carregando",
+        wikipedia: "de Wikipedia, a enciclopédia livre",
+        start: "início"
+    },
+    buttons: {
+        map_overview: "vista geral do mapa",
+        overview: "vista geral",
+        backtostart: "voltar ao início",
+
+        collapse_toggle: "ocultar mapa",
+        uncollapse_toggle: "mostrar mapa"
+    }
+};
