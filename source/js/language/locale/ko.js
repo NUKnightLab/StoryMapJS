265046f71cd04a416134e71c7099477943df4f86
Joe Germuska <joe@germuska.com>
2016-02-01 07:22:09 -0600

    add Korean translation

---
+++ source/js/language/locale/ko.js
@@ -0,0 +1,16 @@
+VCO.Language = {
+    name:                   "한국어",
+    lang:                   "ko",
+    messages: {
+        loading:            "불러오는중",
+        wikipedia:          "위키피디아",
+        start:              "시작하기"
+    },
+    buttons: {
+        map_overview:       "지도 전체보기",
+        overview:           "전체보기",
+        backtostart:        "되돌아가기",
+        collapse_toggle:    "숨기기",
+        uncollapse_toggle:  "보이기"
+    }
+}
