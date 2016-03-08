832af37fbff1efc6defba026cf35760c8da1a893
Joe Germuska <joe@germuska.com>
2016-03-08 13:56:50 -0600

    adding Urdu

---
+++ source/js/language/locale/ur.js
@@ -0,0 +1,16 @@
+VCO.Language = {
+    name:                   "Urdu",
+    lang:                   "ur",
+    messages: {
+        loading:            "لوڈ ہو رہا ہے",
+        wikipedia:          "ویکیپیڈیا, مفت دستیاب انسائیکلوپیڈیا سے ",
+        start:              "دریافت شروع کی جایے "
+    },
+    buttons: {
+        map_overview:       "نقشے کا جائزہ",
+        overview:           "جائزہ",
+        backtostart:        "واپس شروع سے",
+        collapse_toggle:    "نقشہ غائب",
+        uncollapse_toggle:  "نقشہ حاضر"
+    }
+}
