77bd94872cab3da10ab047108e6a36fe71c336b1
Jono Mingard <reason.koan@gmail.com>
2016-09-26 16:38:35 +1300

    Display correct icon on default markers in edit mode

--- static/js/EditorMap.js
+++ static/js/EditorMap.js
@@ -140,13 +140,20 @@ LeafletEditorMap.prototype.removePolyLine = function() {
     }
 }
 
-LeafletEditorMap.prototype.addMarker = function(lat, lng, draggable, customIcon) {
-    var latlng = L.latLng(lat, lng);
+LeafletEditorMap.prototype.addMarker = function(data, draggable) {
+    var latlng = L.latLng(data.location.lat, data.location.lon);
 
-    if (customIcon) {
-      var icon = L.icon({iconUrl: customIcon, iconSize: [48], iconAnchor:[24, 48]});
+    if (data.location.icon) {
+      var icon = L.icon({iconUrl: data.location.icon, iconSize: [48], iconAnchor:[24, 48]});
     } else {
-      var icon = L.divIcon({className: 'vco-mapmarker', iconAnchor:[10, 10]});
+      var media = VCO.MediaType({url: data.media.url});
+      if (media.type) {
+        var iconClass = "vco-mapmarker-icon vco-icon-" + media.type;
+      } else {
+        var iconClass = "vco-mapmarker-icon vco-icon-plaintext";
+      }
+
+      var icon = L.divIcon({className: 'vco-mapmarker ' + iconClass, iconAnchor:[10, 10]});
     }
 
     var marker = L.marker(latlng, {
