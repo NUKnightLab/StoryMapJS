07f0ab4d4498c5f5af2da9668207913fe81e168d
Scott Bradley <scottbradley.mail@gmail.com>
2020-09-01 14:26:59 -0500

    fixes domain in unauthorized message on staging

--- api.py
+++ api.py
@@ -258,7 +258,7 @@ def google_auth_verify():
 
         if 'stg-storymap.knightlab.com' in domains and not info['id'] in allowed_ids:
             print('User id not in ALLOWED_IDS:  %s ' % info['id'])
-            raise Exception('You are not authorized to access this page. Please send the following information to support@knightlab.zendesk.com: storymap.knilab.com unauthorized %s' % info['id'])
+            raise Exception('You are not authorized to access this page. Please send the following information to support@knightlab.zendesk.com: stg-storymap.knightlab.com unauthorized %s' % info['id'])
 
         # Upsert user record
         uid = _get_uid('google:'+info['id'])
