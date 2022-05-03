d8719eb89fdccea987c380aae37a7ab675e68fce
scott2b <@scott2b>
2022-05-03 16:03:35 -0500

    fixes redis host ref

--- storymap/celeryconf.py
+++ storymap/celeryconf.py
@@ -2,5 +2,5 @@ import os
 
 
 redis_host = os.environ.get("REDIS_HOST", "localhost")
-broker_url = f"redis://{REDIS_HOST}:6379/0"
-result_backend = f"redis://{REDIS_HOST}redis:6379/0"
+broker_url = f"redis://{redis_host}:6379/0"
+result_backend = f"redis://{redis_host}redis:6379/0"
