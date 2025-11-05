0f0a848a3b8500ba1e9972c33630b140c61e7aee
Joe Germuska <joe@germuska.com>
2025-11-05 11:11:57 -0600

    Add PNG version of copyright claim image; small tweaks to docker stuff to make local dev work

--- scripts/create-tables.sh
+++ scripts/create-tables.sh
@@ -2,4 +2,4 @@
 
 # uncomment if drop table is needed
 # docker-compose run pg psql -h pg -U storymap -c 'DROP TABLE users;'
-docker-compose run pg psql -h pg -U storymap -c 'CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, uid varchar(32), uname varchar(100), migrated smallint, storymaps jsonb, CONSTRAINT unique_uid UNIQUE (uid));'
+docker compose run pg psql -h pg -U storymap -c 'CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, uid varchar(32), uname varchar(100), migrated smallint, storymaps jsonb, CONSTRAINT unique_uid UNIQUE (uid));'
